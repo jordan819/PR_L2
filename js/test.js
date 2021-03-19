e08bf207f01e564a1921841a2c03712410ddec85
Patryk Zaucha <patryk.zaucha00@gmail.com>
2021-03-19 16:12:10 +0100

    dodanie .js

---
+++ js/test.js
@@ -0,0 +1,9 @@
+console.log();
+for(let i = 1; i < 10; i++){
+	let line='';
+    for(let j = 1; j < 10; j++) {
+		line += (i*j);
+		line += ' ';
+    }
+	console.log(line);
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+let nav = '<nav><a href="index.html">Strona główna</a>' + 
+		' <a href="contact.html">Kontakt</a> </nav>';
+		
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
